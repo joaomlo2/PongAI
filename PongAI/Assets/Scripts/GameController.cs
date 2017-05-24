0932a892e140d97a0811174fc93b8b1d67a10259
João Oliveira <joaomlo2@gmail.com>
2017-05-24 14:50:11 +0000

    First commit

---
+++ PongAI/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController Instance
+	{ 
+		get
+		{
+			if (instance == null) {
+				GameObject aux = Instantiate (new GameObject ()) as GameObject;
+				instance = aux.AddComponent<GameController> ();
+				return instance;
+			}
+			return instance;
+		}
+	}
+	private static GameController instance;
+
+	void Awake()
+	{
+		instance = this;
+		InitGame();
+	}
+
+	void InitGame(){
+		
+	}
+}
